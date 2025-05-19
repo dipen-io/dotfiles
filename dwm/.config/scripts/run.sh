5e1cb2842cf86051a2fd7a809ae493c3e9e3240e
usernamedinesh <borod9200@gmail.com>
2025-05-19 22:37:06 +0530

    add dwm script

---
+++ dwm/.config/scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xrdb merge ~/.Xresources 
+feh --bg-fill ~/Desktop/wall/62.png &
+xset r rate 200 50 &
+brightnessctl s 40% &   # <--- SET BRIGHTNESS TO 40%
+picom &
+# start the bar
+dash ~/.config/bar/bar.sh &
+# start dwm
+while type chadwm >/dev/null; do chadwm && continue || break; done
+
+
