a1539dbc3c333a7c67c6455fd9589ee58a43ac09
usernamedinesh <borod9200@gmail.com>
2026-02-15 10:02:11 +0530

    adding sxhkd for advance keybinds

--- my-nvim/.config/my-nvim/init.lua
+++ my-nvim/.config/my-nvim/init.lua
@@ -8,8 +8,18 @@ require("neovim.core.statusline")
 vim.filetype.add({
   filename = {
     ['init'] = 'sh',
+    ['sxhkdrc'] = 'sxhkdrc',
   },
   pattern = {
     ['.*/river/init'] = 'sh',
   },
 })
+
+-- Add this to your init.lua
+vim.filetype.add({
+  pattern = {
+    -- Matches any file inside ~/.config/i3/config.d/
+    ['.*/.config/i3/config%.d/.*'] = 'i3config',
+  },
+})
+
