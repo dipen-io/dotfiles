ef161cdae9ed84c406d0b592ac5a18aaa81a120a
Dipen Boro <borod9200@gmail.com>
2026-04-08 13:34:42 +0000

    set wallpaper-picker consistent

--- i3/.config/i3/wallpaper-picker.sh
+++ i3/.config/i3/wallpaper-picker.sh
@@ -6,3 +6,4 @@ chosen=$(ls "$WALLPAPER_DIR" | rofi -dmenu -p "🖼 Wallpaper")
 [ -z "$chosen" ] && exit
 
 nitrogen --set-zoom-fill "$WALLPAPER_DIR/$chosen"
+echo "$WALLPAPER_DIR/$chosen" > ~/.config/i3/.current_wallpaper
