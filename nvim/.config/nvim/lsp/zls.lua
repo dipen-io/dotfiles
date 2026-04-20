4819f94051da7e02a565c278f8b4c2cd59906cc7
Dipen Boro <borod9200@gmail.com>
2026-04-20 03:24:44 +0000

    fix zig lsp issue

--- nvim/.config/nvim/lsp/zls.lua
+++ nvim/.config/nvim/lsp/zls.lua
@@ -1,8 +1,26 @@
+-- return {
+--     cmd = { "zls" },  -- or "/usr/local/bin/zls" if needed
+--     filetypes = { "zig" },
+--
+--     root_dir = function()
+--         return vim.fs.dirname(
+--             vim.fs.find({ "zls.json", "build.zig", "zig.mod", ".git" }, { upward = true })[1]
+--         )
+--     end,
+--
+--     settings = {
+--         zls = {
+--             zig_exe_path = "/usr/local/bin/zig", -- fix your error
+--             enable_autofix = true,
+--             enable_snippets = true,
+--             warn_style = true,
+--         },
+--     },
+-- }
+
 return {
-    cmd = { "zls" },     -- or absolute path if needed: "/usr/bin/zls"
+    cmd = { "zls" }, -- or absolute path if needed: "/usr/bin/zls"
     filetypes = { "zig" },
     root_dir = vim.fs.dirname(vim.fs.find({ "zls.json", "build.zig", "zig.mod" }, { upward = true })[1]),
-    settings = {
-        zig_exe_path = "zig",
-    },
 }
+
